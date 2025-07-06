1defca0cef5065061c0126f141e61820cdb08f6c
Kuyho Chung <kchung1995@gmail.com>
2025-07-06 18:23:22 +0900

    refactor: separated wrapReservedWords - made a new object for holding String utilities - moved tests to its corresponding class - separated each assert statement as separate tests

--- processor/src/main/kotlin/io/github/doljae/kotlinlogging/extensions/LoggerProcessor.kt
+++ processor/src/main/kotlin/io/github/doljae/kotlinlogging/extensions/LoggerProcessor.kt
@@ -7,11 +7,54 @@ import com.google.devtools.ksp.processing.Resolver
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import io.github.doljae.common.StringUtility.wrapReservedWords
 
 class LoggerProcessor(
     private val codeGenerator: CodeGenerator,
     private val logger: KSPLogger,
 ) : SymbolProcessor {
+    override fun process(resolver: Resolver): List<KSAnnotated> {
+        val classes =
+            resolver
+                .getNewFiles()
+                .flatMap { file -> file.declarations.filterIsInstance<KSClassDeclaration>() }
+
+        classes.forEach { classDeclaration ->
+            generateLogger(classDeclaration)
+        }
+
+        return emptyList()
+    }
+
+    private fun generateLogger(classDeclaration: KSClassDeclaration) {
+        val className = classDeclaration.simpleName.asString()
+        val loggerCode =
+            """
+            package ${classDeclaration.packageName.asString()}
+            
+            import io.github.oshai.kotlinlogging.KLogger
+            import io.github.oshai.kotlinlogging.KotlinLogging
+            
+            val $className.log: KLogger
+                get() = KotlinLogging.logger("${classDeclaration.qualifiedName!!.asString()}")
+            """.trimIndent()
+
+        codeGenerator
+            .createNewFile(
+                Dependencies(false),
+                wrapReservedWords(
+                    target = classDeclaration.packageName.asString(),
+                    delimiter = '.',
+                    reservedWords = hardKeywords,
+                    quoteChar = '`',
+                ),
+                "${className}KotlinLoggingExtensions",
+            ).bufferedWriter()
+            .use { writer ->
+                writer.write(loggerCode)
+            }
+    }
+
     companion object {
         // Ref: https://kotlinlang.org/docs/keyword-reference.html
         private val hardKeywords =
@@ -45,58 +88,5 @@ class LoggerProcessor(
                 "when",
                 "while",
             )
-
-        fun String.wrapReservedWords(
-            delimiter: Char,
-            reservedWords: Collection<String>,
-            quoteChar: Char,
-        ): String {
-            val tokens = split(delimiter)
-            return tokens.joinToString(delimiter.toString()) { token ->
-                if (reservedWords.contains(token)) {
-                    "$quoteChar$token$quoteChar"
-                } else {
-                    token
-                }
-            }
-        }
-    }
-
-    override fun process(resolver: Resolver): List<KSAnnotated> {
-        val classes =
-            resolver
-                .getNewFiles()
-                .flatMap { file -> file.declarations.filterIsInstance<KSClassDeclaration>() }
-
-        classes.forEach { classDeclaration ->
-            generateLogger(classDeclaration)
-        }
-
-        return emptyList()
-    }
-
-    private fun generateLogger(classDeclaration: KSClassDeclaration) {
-        val className = classDeclaration.simpleName.asString()
-        val loggerCode =
-            """
-            package ${classDeclaration.packageName.asString()}
-            
-            import io.github.oshai.kotlinlogging.KLogger
-            import io.github.oshai.kotlinlogging.KotlinLogging
-            
-            val $className.log: KLogger
-                get() = KotlinLogging.logger("${classDeclaration.qualifiedName!!.asString()}")
-            """.trimIndent()
-
-        codeGenerator
-            .createNewFile(
-                Dependencies(false),
-                classDeclaration.packageName.asString()
-                    .wrapReservedWords(delimiter = '.', reservedWords = keywords, quoteChar = '`'),
-                "${className}KotlinLoggingExtensions",
-            ).bufferedWriter()
-            .use { writer ->
-                writer.write(loggerCode)
-            }
     }
 }
--- processor/src/test/kotlin/io/github/doljae/kotlinlogging/extensions/LoggerProcessorTest.kt
+++ processor/src/test/kotlin/io/github/doljae/kotlinlogging/extensions/LoggerProcessorTest.kt
@@ -1,8 +1,6 @@
 package io.github.doljae.kotlinlogging.extensions
 
-import io.github.doljae.kotlinlogging.extensions.LoggerProcessor.Companion.wrapReservedWords
 import kotlin.test.Test
-import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class LoggerProcessorTest {
@@ -23,81 +21,5 @@ class LoggerProcessorTest {
 
         val processorName = LoggerProcessor::class.simpleName
         assertTrue(processorName == "LoggerProcessor", "Processor class has correct name")
-    }
-
-    @Test
-    fun `package name should handle reserved words correctly`() {
-        // A subset of Kotlin keywords to test the wrapping logic,
-        // mirroring the keywords in LoggerProcessor.
-        val reservedWords =
-            setOf(
-                "as",
-                "break",
-                "class",
-                "continue",
-                "do",
-                "else",
-                "for",
-                "fun",
-                "if",
-                "in",
-                "is",
-                "null",
-                "object",
-                "package",
-                "return",
-                "super",
-                "this",
-                "throw",
-                "try",
-                "typealias",
-                "typeof",
-                "val",
-                "var",
-                "when",
-                "while",
-            )
-        val delimiter = '.'
-        val quoteChar = '`'
-
-        // Test case 1: Package name with a common reserved word "in"
-        val packageName1 = "com.katfun.tech.share.sample.codes.in"
-        val expected1 = "com.katfun.tech.share.sample.codes.`in`"
-        assertEquals(expected1, packageName1.wrapReservedWords(delimiter, reservedWords, quoteChar))
-
-        // Test case 2: Package name with multiple reserved words
-        val packageName2 = "package.io.github.class.for.val"
-        val expected2 = "`package`.io.github.`class`.`for`.`val`"
-        assertEquals(expected2, packageName2.wrapReservedWords(delimiter, reservedWords, quoteChar))
-
-        // Test case 3: Package name with no reserved words
-        val packageName3 = "org.example.application.utils"
-        val expected3 = "org.example.application.utils"
-        assertEquals(expected3, packageName3.wrapReservedWords(delimiter, reservedWords, quoteChar))
-
-        // Test case 4: Package name where part of a token contains a reserved word (should not wrap)
-        val packageName4 = "my.classy.stuff.inside.values"
-        val expected4 = "my.classy.stuff.inside.values"
-        assertEquals(expected4, packageName4.wrapReservedWords(delimiter, reservedWords, quoteChar))
-
-        // Test case 5: Empty package name
-        val packageName5 = ""
-        val expected5 = ""
-        assertEquals(expected5, packageName5.wrapReservedWords(delimiter, reservedWords, quoteChar))
-
-        // Test case 6: Package name that is a single reserved word
-        val packageName6 = "class"
-        val expected6 = "`class`"
-        assertEquals(expected6, packageName6.wrapReservedWords(delimiter, reservedWords, quoteChar))
-
-        // Test case 7: Package name with reserved word at the beginning
-        val packageName7 = "fun.project.app"
-        val expected7 = "`fun`.project.app"
-        assertEquals(expected7, packageName7.wrapReservedWords(delimiter, reservedWords, quoteChar))
-
-        // Test case 8: Package name with reserved word at the end and no others
-        val packageName8 = "another.example.object"
-        val expected8 = "another.example.`object`"
-        assertEquals(expected8, packageName8.wrapReservedWords(delimiter, reservedWords, quoteChar))
     }
 }
