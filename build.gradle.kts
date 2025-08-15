d8926459dbde20c1cd0b56a383b8c792f96c990f
Seokjae Lee <seok9211@naver.com>
2025-08-15 10:00:15 +0900

    build: bump dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.1.21" apply false
-    id("com.google.devtools.ksp") version "2.1.21-2.0.2" apply false
+    kotlin("jvm") version "2.2.10" apply false
+    id("com.google.devtools.ksp") version "2.2.10-2.0.2" apply false
     id("com.vanniktech.maven.publish") version "0.34.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "13.0.0" apply false
 }
