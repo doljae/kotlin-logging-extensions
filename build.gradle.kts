705ec66cceb62b2865cacffd8db3eb478de87056
Seokjae Lee <seokjae.lee@naverlabs.com>
2026-03-31 11:18:02 +0200

    build: bump dependency version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,10 @@ import org.gradle.kotlin.dsl.configure
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
 
 plugins {
-    kotlin("jvm") version "2.3.10" apply false
+    kotlin("jvm") version "2.3.20" apply false
     id("com.google.devtools.ksp") version "2.3.6" apply false
     id("com.vanniktech.maven.publish") version "0.36.0" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "14.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
 }
 
 description = "kotlin-logging-extensions"
